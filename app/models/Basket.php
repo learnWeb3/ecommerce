c44c2b21967783eb235de51d57cba0a90c727355
antoine <antoine.le-guillou@laplateforme.io>
2020-08-30 17:23:16 +0200

    adding basket fetch content

--- app/models/Basket.php
+++ app/models/Basket.php
@@ -29,7 +29,27 @@ class Basket
     public function getContent()
     {
         $connection = Db::connect();
-        $statement = "SELECT * FROM baskets 
+        $statement = 
+        
+        "SELECT 
+            -- CATEGORIES
+            categories.id as category_id,
+            categories.name as category_name,
+            categories.created_at as category_created_at,
+            categories_updated_at as category_updated_at,
+            -- BOOKS
+            books.id as book_id,
+            books.created_at as book_created_at,
+            books.updated_at as book_updated_at
+            books.title as book_title,
+            books.author as book_author,
+            books.collection as books_collection,
+            books.description as book_description,
+            books.price as book_price,
+            books.publication_year as book_publication_year
+            books.image_path as book_image_path,
+            books.category_id as book_category_id,
+        FROM baskets 
             JOIN users ON baskets.user_id=users.id 
             JOIN books ON books.id=baskets.book_id 
             JOIN states ON states.id=baskets.state_id 
@@ -37,10 +57,17 @@ class Basket
 
         $query = $connection->query($statement);
 
+        $results = [];
         while($row = $query->fetch())
         {
-            $results[]= new Book($row['title'],$row['author'],$row['collection'],$row['price'],$row['publication_year'], $row['image_path'],$row['descripton'],$row['id'],$row['created_at'],$row['updated_at'])
+            $results[]= array(
+                "book"=>new Book($row['book_title'],$row['book_author'],$row['book_collection'],$row['book_price'],$row['book_publication_year'], $row['book_image_path'],$row['book_descripton'],$row['book_id'],$row['book_created_at'],$row['book_updated_at']),
+                "category"=>new Category($row['category_name'])
+            );
+
         }
+
+        return $results;
 
         
     }
