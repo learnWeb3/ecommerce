45cc4d1a444d5205480c177c190084f68b259ddb
antoine <antoine.le-guillou@laplateforme.io>
2020-09-07 15:15:15 +0200

    linking sign in up page in toggle nav

--- app/layouts/navbar.php
+++ app/layouts/navbar.php
@@ -8,7 +8,7 @@
 
 
         <ul id="nav-brand-container">
-            <li><img src="<?php echo ABSOLUTE_ASSET_PATH . "/icons/logo/logo.svg" ?>" alt="logo icon" class="nav-brand"></li>
+            <li><a href="<?php echo REDIRECT_BASE_URL."controller=home&method=index" ?>"><img src="<?php echo ABSOLUTE_ASSET_PATH . "/icons/logo/logo.svg" ?>" alt="logo icon" class="nav-brand"></a></li>
         </ul>
 
 
@@ -27,6 +27,12 @@
         </ul>
 
         <ul>
+            <li class="mr-4 relative"><img src="<?php echo ABSOLUTE_ASSET_PATH . "/icons/action/user.svg" ?>" id="user_sign_action" alt="user icon">
+                <ul class="toogle d-none">
+                    <li><a href="<?php echo REDIRECT_BASE_URL."controller=session&method=new"?>">Connexion</a></li>
+                    <li><a href="<?php echo REDIRECT_BASE_URL."controller=user&method=new"?>">Inscription</a></li>
+                </ul>
+            </li>
             <li id="search-open" class="mr-4"><img src="<?php echo ABSOLUTE_ASSET_PATH . "/icons/action/search.svg" ?>" alt="search icon" title="rechercher un produit"></li>
             <li id="shopping-cart"><img src="<?php echo ABSOLUTE_ASSET_PATH . "/icons/action/shopping_cart.svg" ?>" alt="logo icon" title="voir le panier">
                 <div class="badge-shopping-cart"></div>
@@ -51,7 +57,7 @@
 
 <section class="autocomplete-zone">
     <div class="block">
-       
+
 
     </div>
     <img src="<?php echo ABSOLUTE_ASSET_PATH . "/icons/action/chevron_left.svg" ?>" alt="up arrow button" class="up">
@@ -61,4 +67,13 @@
 <script>
     Autocomplete.search();
     Autocomplete.activateNavigation();
+
+    $("#user_sign_action").click(function() {
+        var toogle = $(this).parents('li').find("ul.toogle");
+        if (toogle.attr('class') == "toogle d-none") {
+            toogle.removeClass("d-none").addClass("d-flex");
+        } else {
+            toogle.removeClass("d-flex").addClass("d-none");
+        }
+    });
 </script>
