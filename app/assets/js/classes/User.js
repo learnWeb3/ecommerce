022060155d39186879cdc30917439de35e7254ea
antoine <antoine.le-guillou@laplateforme.io>
2020-09-12 12:16:19 +0200

    adding working alerts signup

--- app/assets/js/classes/User.js
+++ app/assets/js/classes/User.js
@@ -1,12 +1,10 @@
-class User
-{
-    constructor(email,password)
-    {
+class User {
+    constructor(email, password) {
         this.email = email;
-        this.password=password;
+        this.password = password;
     }
 
-    static create(targetedSelector){
+    static create(targetedSelector) {
         $(targetedSelector).click(function () {
 
             $(targetedSelector).parent("form").submit(function (event) {
@@ -19,16 +17,15 @@ class User
                     data: "controller=user&method=create&" + $(this).serialize() + "&remote=true",
                     dataType: "JSON",
                     success: function (results, status) {
-                        if (results.hasOwnProperty("type") && results.type == "danger")
-                        {
-                            $("#sign-container").append("<div id='alert' class='"+results.type+"'><img src='app/assets/icons/navigation/close.svg' alt='' id='close'></div>");
-                            
-                            results.message.foreach(function(e){
-                                $("#alert").append("<p>"+e+"</p>")
-                            });
+                        if (results.hasOwnProperty("type") && results.type == "danger") {
 
-                           
-                
+                            if ($('#alert').length > 0) {$("#alert").remove();}
+                            $("#sign-container").append("<div id='alert' class='" + results.type + "'><img src='app/assets/icons/navigation/close.svg' alt='' id='close'></div>");
+                            let alert = $("#alert");
+                            results.message.forEach(element => {
+                                alert.append("<p>" + element + "</p>")
+                            });
+                            $("#alert #close").click(function(){$("#alert").remove()});
                         }
                     },
                     error: function (XhrObject, error, status) {
@@ -42,17 +39,16 @@ class User
 
     }
 
-    static destroy(){
+    static destroy() {
         $.ajax({
-            url:"/ecommerce/index.php",
-            method:"POST",
-            data:"controller=user&method=destroy&" + $(this).serialize() + "&remote=true",
-            dataType:"JSON",
-            success:function(results,status){
+            url: "/ecommerce/index.php",
+            method: "POST",
+            data: "controller=user&method=destroy&" + $(this).serialize() + "&remote=true",
+            dataType: "JSON",
+            success: function (results, status) {
                 console.log(results);
             },
-            error:function(XhrObject,error,status)
-            {
+            error: function (XhrObject, error, status) {
                 console.log(error);
             }
         });
