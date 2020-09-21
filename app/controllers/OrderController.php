1eda61d1d6e3faf433a63f8d41d829938f1bbd07
antoine <antoine.le-guillou@laplateforme.io>
2020-09-21 10:11:16 +0200

    updating checkout process

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -1,3 +1,4 @@
+
 <?php
 
 class OrderController extends ApplicationController
@@ -12,9 +13,17 @@ class OrderController extends ApplicationController
     {
         if (isset($_GET['step'])) {
             if ($_GET['step'] == "1") {
+                $meta_title = "La Nuit des Temps: nouvelle commande - connexion";
+                $meta_description = "1/3 Nouvelle commande: identification";
             } elseif ($_GET['step'] == "2") {
+                $meta_title = "La Nuit des Temps: nouvelle commande - votre addresse";
+                $meta_description = "2/3 Nouvelle commande: votre addresse";
             } elseif ($_GET['step'] == "3") {
+                $meta_title = "La Nuit des Temps: nouvelle commande - paiement";
+                $meta_description = "3/3 Nouvelle commande: paiement";
             }
         }
+
+        $this->render("index", $meta_title, $meta_description, array());
     }
 }
---
+++ app/views/order/index.php
@@ -0,0 +1,130 @@
+<?php if (isset($_GET['step'])) : ?>
+
+
+    <?php if ($_GET['step'] == "1") : ?>
+
+
+        <section class="container justify-content-center my-5" style="min-height:100vh">
+
+            <div class="row divide-xl-1 divide-lg-1 divide-md-1 divide-sm-1 divide-xs-1" style="min-height:unset">
+
+                <div class="col" id="sign-in-container">
+
+                    <h1 class="text-center">Bonjour</h1>
+
+                    <h2 class="text-center"><small class="my-4">(champs obligatoires *)</small></h2>
+
+                    <hr class="light my-2">
+
+
+                    <form action="<?php echo REDIRECT_BASE_URL . "controller=session&method=create" ?>" method="post" id="sign-in">
+
+                        <div class="form-group">
+                            <label for="">Adresse email *</label>
+                            <input type="email" name="user_email" id="user_email" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Mot de passe *</label>
+                            <input type="password" name="user_password" id="user_password" required>
+                        </div>
+
+
+                        <button class="btn btn-lg btn-success my-4" type="submit">connexion</button>
+                    </form>
+                </div>
+            </div>
+
+            <div class="row divide-xl-1 divide-lg-1 divide-md-1 divide-sm-1 divide-xs-1" style="min-height:unset">
+
+                <div class="col" id="sign-up-container">
+
+                    <h2 class="text-center">Je suis nouveau ici</h2>
+
+                    <hr class="light my-2">
+
+                    <button class="btn btn-lg btn-primary my-4" id="checkout-signup">inscription </button>
+
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+    <?php elseif ($_GET['step'] == "2") : ?>
+
+
+    <?php elseif ($_GET['step'] == "3") : ?>
+
+    <?php endif; ?>
+
+
+<?php endif; ?>
+
+<script>
+    function getFormSignIn() {
+        return "<form action='index.php?controller=session&method=create' method='post' id='sign-in'>" +
+
+            "<div class='form-group'>" +
+            "<label for=''>Adresse email *</label>" +
+            "<input type='email' name='user_email' id='user_email' required>" +
+            "</div>" +
+
+            "<div class='form-group'>" +
+            "<label for=''>Mot de passe *</label>" +
+            "<input type='password' name='user_password' id='user_password' required>" +
+            "</div>" +
+
+            "<button class='btn btn-lg btn-success my-4' type='submit'>connexion</button>" +
+            "</form>"
+    }
+
+    function getFormSignUp() {
+        return "<form action='index.php?controller=user&method=create' method='post' id='sign-up'>" +
+
+            "<div class='form-group'>" +
+            "<label for='user_email'>Adresse email *</label>" +
+            "<input type='email' name='user_email' id='user_email' required>" +
+            "</div>" +
+
+            "<div class='form-group'>" +
+            "<label for='user_password'>Mot de passe *</label>" +
+            "<input type='password' name='user_password' id='user_password' required>" +
+            "</div>" +
+
+            "<div class='form-group'>" +
+            "<label for='user_password_confirmation'>Confirmer le mot de passe *</label>" +
+            "<input type='password' name='user_password_confirmation' id='user_password_confirmation' required>" +
+            '</div>' +
+
+            "<button class='btn btn-lg btn-primary my-4' type='submit'>incription</button>" +
+            "</form>";
+    }
+
+    function signUpToggle() {
+        $("#checkout-signup").click(function() {
+            $(this).remove();
+            $("#sign-in-container").find("form").remove();
+            $("#sign-up-container").append(getFormSignUp())
+            $("#sign-in-container").append("<button class='btn btn-lg btn-success my-4' id='checkout-signin'>connexion</button>");
+            signInToggle();
+        });
+
+    }
+
+
+
+    function signInToggle() {
+        $("#checkout-signin").click(function() {
+            $(this).remove();
+            $("#sign-up-container").find("form").remove();
+            $("#sign-in-container").append(getFormSignIn())
+            $("#sign-up-container").append("<button class='btn btn-lg btn-primary my-4' id='checkout-signup'>inscription </button>");
+            signUpToggle();
+        });
+    }
+
+    signUpToggle();
+</script>
