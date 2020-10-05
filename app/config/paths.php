d8caffd5d6e76938638385969c2d83826a6b938e
Sabri SAID <sabri.said@laplateforme.io>
2020-10-05 17:40:45 +0200

    Create mail template
    Mail template for signup & order confirmation

--- app/config/paths.php
+++ app/config/paths.php
@@ -1,5 +1,7 @@
 <?php
-define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT'] ."/". "ecommerce");
+
+var_dump($_SERVER);
+define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT']);
 define("APP_PATH", ROOT_PATH . "/app");
 define("CONTROLLER_PATH", APP_PATH . "/controllers");
 define("VIEW_PATH", APP_PATH . "/views");
@@ -11,5 +13,10 @@ define("ERROR_PATH", APP_PATH . "/layouts/errors");
 define("UPLOAD_PATH", APP_PATH."/public/upload");
 define("ABSOLUTE_UPLOAD_PATH", "http://" . $_SERVER["SERVER_NAME"] ."/ecommerce/app/public/upload");
 
-define("ABSOLUTE_ASSET_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/ecommerce/app/assets");
-define("REDIRECT_BASE_URL",  "http://" . $_SERVER["SERVER_NAME"] . "/ecommerce/index.php?" );
+if (ROOT_PATH == '/var/www/ecommerce') {
+    define("ABSOLUTE_ASSET_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/app/assets");
+    define("REDIRECT_BASE_URL", "http://" . $_SERVER["SERVER_NAME"] . "/index.php?");
+} else {
+    define("ABSOLUTE_ASSET_PATH", "http://" . $_SERVER["SERVER_NAME"] . "ecommerce/app/assets");
+    define("REDIRECT_BASE_URL", "http://" . $_SERVER["SERVER_NAME"] . "ecommerce/index.php?");
+}
