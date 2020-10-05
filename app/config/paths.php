73f18fd066e75051a035fd78fc42fa8a87fa72c3
Sabri SAID <sabri.said@laplateforme.io>
2020-10-05 17:46:43 +0200

    Update path
    Path according to document root user

--- app/config/paths.php
+++ app/config/paths.php
@@ -1,7 +1,17 @@
 <?php
 
-var_dump($_SERVER);
-define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT']);
+if (ROOT_PATH == '/var/www/ecommerce') {
+    define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT']);
+    define("ABSOLUTE_ASSET_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/app/assets");
+    define("REDIRECT_BASE_URL", "http://" . $_SERVER["SERVER_NAME"] . "/index.php?");
+    define("ABSOLUTE_UPLOAD_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/app/public/upload");
+} else {
+    define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT'] . "/" . "ecommerce");
+    define("ABSOLUTE_ASSET_PATH", "http://" . $_SERVER["SERVER_NAME"] . "ecommerce/app/assets");
+    define("REDIRECT_BASE_URL", "http://" . $_SERVER["SERVER_NAME"] . "ecommerce/index.php?");
+    define("ABSOLUTE_UPLOAD_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/ecommerce/app/public/upload");
+}
+
 define("APP_PATH", ROOT_PATH . "/app");
 define("CONTROLLER_PATH", APP_PATH . "/controllers");
 define("VIEW_PATH", APP_PATH . "/views");
@@ -10,13 +20,4 @@ define("SERVICE_PATH", APP_PATH . "/services");
 define("HELPER_PATH", APP_PATH . "/helpers");
 define("LAYOUT_PATH", APP_PATH . "/layouts");
 define("ERROR_PATH", APP_PATH . "/layouts/errors");
-define("UPLOAD_PATH", APP_PATH."/public/upload");
-define("ABSOLUTE_UPLOAD_PATH", "http://" . $_SERVER["SERVER_NAME"] ."/ecommerce/app/public/upload");
-
-if (ROOT_PATH == '/var/www/ecommerce') {
-    define("ABSOLUTE_ASSET_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/app/assets");
-    define("REDIRECT_BASE_URL", "http://" . $_SERVER["SERVER_NAME"] . "/index.php?");
-} else {
-    define("ABSOLUTE_ASSET_PATH", "http://" . $_SERVER["SERVER_NAME"] . "ecommerce/app/assets");
-    define("REDIRECT_BASE_URL", "http://" . $_SERVER["SERVER_NAME"] . "ecommerce/index.php?");
-}
+define("UPLOAD_PATH", APP_PATH . "/public/upload");
