064d684e02c1af93ce898a191ee8af3ae01a9dde
antoine-leguillou <62133905+antoine-leguillou@users.noreply.github.com>
2020-10-06 13:20:21 +0200

    Update paths.php

--- app/config/paths.php
+++ app/config/paths.php
@@ -22,18 +22,4 @@ define("LAYOUT_PATH", APP_PATH . "/layouts");
 define("ERROR_PATH", APP_PATH . "/layouts/errors");
 define("UPLOAD_PATH", APP_PATH . "/public/upload");
 
-// define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT'] ."/". "ecommerce");
-// define("APP_PATH", ROOT_PATH . "/app");
-// define("CONTROLLER_PATH", APP_PATH . "/controllers");
-// define("VIEW_PATH", APP_PATH . "/views");
-// define("MODEL_PATH", APP_PATH . "/models");
-// define("SERVICE_PATH", APP_PATH . "/services");
-// define("HELPER_PATH", APP_PATH . "/helpers");
-// define("LAYOUT_PATH", APP_PATH . "/layouts");
-// define("ERROR_PATH", APP_PATH . "/layouts/errors");
-// define("UPLOAD_PATH", APP_PATH."/public/upload");
-// define("ABSOLUTE_UPLOAD_PATH", "http://" . $_SERVER["SERVER_NAME"] ."/ecommerce/app/public/upload");
-
-// define("ABSOLUTE_ASSET_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/ecommerce/app/assets");
-// define("REDIRECT_BASE_URL",  "http://" . $_SERVER["SERVER_NAME"] . "/ecommerce/index.php?" );
 
