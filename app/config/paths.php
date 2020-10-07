25e5f77c051dfc5c48a89a232f52adc1a0e1cba0
antoine-leguillou <62133905+antoine-leguillou@users.noreply.github.com>
2020-10-07 18:39:35 +0200

    adding comment on moved stripe redirection path

--- app/config/paths.php
+++ app/config/paths.php
@@ -5,11 +5,16 @@ if ($_SERVER['DOCUMENT_ROOT'] == '/var/www/ecommerce') {
     define("ABSOLUTE_ASSET_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/app/assets");
     define("REDIRECT_BASE_URL", "http://" . $_SERVER["SERVER_NAME"] . "/index.php?");
     define("ABSOLUTE_UPLOAD_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/app/public/upload");
+    // DEFINE STRIPE URL 
+    
 } else {
     define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT'] . "/" . "ecommerce");
     define("ABSOLUTE_ASSET_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/ecommerce/app/assets");
     define("REDIRECT_BASE_URL", "http://" . $_SERVER["SERVER_NAME"] . "/ecommerce/index.php?");
     define("ABSOLUTE_UPLOAD_PATH", "http://" . $_SERVER["SERVER_NAME"] . "/ecommerce/app/public/upload");
+    define("STRIPE_SUCCESS_URL", "http://".$_SERVER['SERVER_NAME']."/ecommerce/index.php?controller=checkout&method=success");
+    define("STRIPE_CANCEL_URL", "http://".$_SERVER['SERVER_NAME']."/ecommerce/index.php?controller=checkout&method=error");
+
 }
 
 define("APP_PATH", ROOT_PATH . "/app");
--- app/config/stripe_credentials.php
+++ app/config/stripe_credentials.php
@@ -1,6 +1,3 @@
 <?php 
-
 define("STRIPE_PUBLISHABLE_KEY", 'pk_test_51HKRyVBCSIgE2GxuO27KfzF7hek10vPdCAsNm1xy0MUemMPplqHD0QWGy2XCiIrfqu5zLka5ZuTo6ZViEMfVMHTz00XErL6cYA');
 define("STRIPE_SECRET_KEY", "sk_test_51HKRyVBCSIgE2Gxu1J5IqjK3yh4Wq4nlVmAY8ywa8AO7dRPaW4RsLD7l2tQHgkD95IsBWSzuLsLO1A5CwIOLKPCZ00dyq8AlUX");
-define("STRIPE_SUCCESS_URL", "http://".$_SERVER['SERVER_NAME']."/ecommerce/index.php?controller=checkout&method=success");
-define("STRIPE_CANCEL_URL", "http://".$_SERVER['SERVER_NAME']."/ecommerce/index.php?controller=checkout&method=error");
