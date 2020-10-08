852e28ed3a946079a1686e71b0c0a89b91276fb4
antoine-leguillou <62133905+antoine-leguillou@users.noreply.github.com>
2020-10-08 09:20:49 +0200

    Update Mailer.php

--- app/services/Mailer.php
+++ app/services/Mailer.php
@@ -35,46 +35,43 @@ class Mailer
     public function send(array $vars = [])
     {
         extract($vars);
-//        ob_start();
-//        require_once $this->message_path;
-//        $html = ob_get_clean();
-//        $sendgrid_apikey = "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";
-//
-//        $url = 'https://api.sendgrid.com/';
-//
-//        $params = array(
-//            'to'        => "test@yopmail.com",
-//            'toname'    => "Example User",
-//            'from'      =>  $this->sender_email,
-//            'fromname'  => "lndt",
-//            'subject'   => $this->subject,
-//            'text'      => "I'm text!",
-//            'html'      => $html,
-//        );
-//
-//        $request =  $url . 'api/mail.send.json';
-//
-//        // Generate curl request
-//        $session = curl_init($request);
-//
-//        $options = array(
-//            CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2,
-//            CURLOPT_POST => true,
-//            CURLOPT_RETURNTRANSFER => true,
-//            CURLOPT_HTTPHEADER => array('Authorization: Bearer ' . $sendgrid_apikey),
-//            CURLOPT_POSTFIELDS => $params
-//        );
-//        curl_setopt_array($session, $options);
-//
-//        // obtain response
-//        $response = curl_exec($session);
-//        curl_close($session);
-//
-//        // print everything out
-//        print_r($response);
+       ob_start();
+       require_once $this->message_path;
+       $html = ob_get_clean();
+       $sendgrid_apikey = "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";
 
-        require_once $this->message_path;
-        die();
+       $url = 'https://api.sendgrid.com/';
+
+       $params = array(
+           'to'        => "test@yopmail.com",
+           'toname'    => "Example User",
+           'from'      =>  $this->sender_email,
+           'fromname'  => "lndt",
+           'subject'   => $this->subject,
+           'text'      => "I'm text!",
+           'html'      => $html,
+       );
+
+       $request =  $url . 'api/mail.send.json';
+
+       // Generate curl request
+       $session = curl_init($request);
+
+       $options = array(
+           CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2,
+           CURLOPT_POST => true,
+           CURLOPT_RETURNTRANSFER => true,
+           CURLOPT_HTTPHEADER => array('Authorization: Bearer ' . $sendgrid_apikey),
+           CURLOPT_POSTFIELDS => $params
+       );
+       curl_setopt_array($session, $options);
+
+       // obtain response
+       $response = curl_exec($session);
+       curl_close($session);
+
+       // print everything out
+       print_r($response);
     }
 }
 
