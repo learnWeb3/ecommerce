04e16b68335df6fcdaa4b2feae8c5340fd5df5db
antoine-leguillou <62133905+antoine-leguillou@users.noreply.github.com>
2020-10-09 14:22:04 +0200

    adding last version

--- app/layouts/admin_template.php
+++ app/layouts/admin_template.php
@@ -45,13 +45,6 @@
         <?php echo  $yield ?>
     </main>
 
-<<<<<<< HEAD
-    <?php //require_once LAYOUT_PATH . "/footer_admin.php" ?>
-=======
-    <?php require_once LAYOUT_PATH . "/footer.php" ?>
->>>>>>> parent of 941e69f... changing errors layouts
-
-
     <script>
         $(document).ready(function() {
             appendArrowInputNumber();
